f8333fd641d20b752f6d3749a69f481e41be3f0b
jisu <tjdwltn121@gmail.com>
2025-01-13 14:19:42 +0900

    feat: 번들 최적화, bundle-분석 추가

--- packages/next-pr/next.config.js
+++ packages/next-pr/next.config.js
@@ -1,20 +1,52 @@
-const Dotenv = require('dotenv-webpack');
 const {join} = require('path');
+const {NextFederationPlugin} = require('@module-federation/nextjs-mf');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true', // ANALYZE 환경변수 활성화 시 동작
+});
 
-module.exports = {
+dotenv.config({path: join(__dirname, `../../.env.${process.env.NODE_ENV}`)});
+
+const config = withBundleAnalyzer({
 	output: 'standalone',
-	reactStrictMode: true,
-	pageExtensions: ['js', 'jsx'],
+	reactStrictMode: false,
+	swcMinify: true,
+	optimization: {
+		runtimeChunks: true,
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+	images: {domains: ['*'], minimumCacheTTL: 60},
+	sassOptions: {includePaths: [join(__dirname, 'styles')]},
+	webpack: (config, {isServer}) => {
+		if (!isServer) {
+			config.resolve.fallback.dns = false;
+			config.resolve.fallback.net = false;
+			config.resolve.fallback.tls = false;
 
-	webpack: (config) => {
-		config.plugins.push(new Dotenv({path: `../../config/.env.development`}));
+			config.plugins.push(
+				new NextFederationPlugin({
+					name: 'client',
+					filename: 'static/chunks/remoteEntry.js',
+					shared: {
+						'next/navigation': {singleton: true},
+						'next/router': {singleton: true},
+						recoil: {singleton: true},
+						react: {singleton: true},
+					},
+				}),
+			);
+		}
+
 		return config;
 	},
-	images: {
-		domains: ['localhost', '*'],
-		minimumCacheTTL: 60,
+	publicRuntimeConfig: {},
+	experimental: {
+		optimizePackageImports: [],
 	},
-	sassOptions: {
-		includePaths: [join(__dirname, 'styles')],
-	},
-};
+	transpilePackages: [],
+});
+
+module.exports = config;
